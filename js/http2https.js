fbbf56f620dc4f7d6548410c732dde715c32f456
Pavel Frolov <tigrashik@gmail.com>
2017-05-22 08:54:56 +0300

    http2https.js

---
+++ js/http2https.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+  $("a").each(function() {
+    var i = $(this).attr("href");
+    var n = i.replace("http://blog.radjah", "https://blog.radjah");
+    $(this).attr("href", function() {
+    return n
+    })
+  })
+});
